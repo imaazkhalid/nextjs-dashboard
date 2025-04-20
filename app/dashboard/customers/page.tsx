34b4482be894f96c00cec2efa162713baf70ab58
Maaz Khalid <imaazkhalid@gmail.com>
2025-04-20 20:24:32 +0500

    update customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,21 @@
-export default function Page() {
-    return <p>Customers Page</p>
+import CustomersTable from '@/app/ui/customers/table';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+
+export default async function Page(
+  props: {
+    searchParams?: Promise<{
+      query?: string;
+    }>;
+  }
+) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <div className="w-full">
+      <h1 className="text-2xl">Customers</h1>
+      <CustomersTable customers={customers} />
+    </div>
+  );
 }
